2258941990bd2c472a22176d03c73e6d68164b54
Brandon Smith <brandon.smith@g3ti.net>
2023-06-23 14:12:17 +0000

    Added SPEC file for RPM packaging

--- setup.py
+++ setup.py
@@ -5,35 +5,12 @@ from setuptools.command.install import install
 import os
 import pwd
 
-class PostDevelopCommand(develop):
-    """Post-installation for development mode."""
-    def run(self):
-        develop.run(self)
-        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        install.run(self)
-        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-        path = '/home/ndruser/Desktop/NDR562_Releases/'
-        isFile = os.path.isdir(path)
-        if isFile == False:
-            try:
-                os.mkdir(path)
-            except OSError:
-                print ("Creation of the directory %s failed" % path)
-            else:
-                print ("Successfully created the directory %s " % path)
-
 VERSION='20.03.12'
 
-DATA_FILES = [('/usr/share/applications/',['NDR562CommandLine.Desktop'])]
 SCRIPTS = ['ndrcommandline.py']
 
 setup(name='NDRXXXCommandLine',
       version=VERSION,
       packages=None,
-      data_files=DATA_FILES,
       scripts=SCRIPTS
       )
